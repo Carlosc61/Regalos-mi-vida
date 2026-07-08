a932264fa14bee0b9bc7248191817f60f066227e
chavezramoscarlos440 <chavezramoscarlos440@gmail.com>
2026-07-08 16:50:57 -0500

    Corregir formato de fecha

--- login.js
+++ login.js
@@ -1,56 +1,42 @@
-const fechaCorrecta = "2020-03-03"; // Formato YYYY-MM-DD
+const fechaCorrecta = "2020-03-03";
 
 function verificar() {
 
     const fecha = document.getElementById("fecha").value;
 
+    alert("La fecha seleccionada es: " + fecha);
+
     const estado = document.getElementById("estado");
-
     const barra = document.querySelector(".barra");
-
     const progreso = document.getElementById("progreso");
 
-    // Reiniciar barra
     progreso.style.width = "0%";
     barra.style.display = "none";
 
     if (fecha !== fechaCorrecta) {
-
         estado.style.color = "red";
         estado.innerHTML = "❌ Fecha incorrecta<br>Ese recuerdo aún no abre este universo.";
-
-        if (navigator.vibrate) {
-            navigator.vibrate(200);
-        }
-
         return;
     }
 
     estado.style.color = "cyan";
-    estado.innerHTML = "🔍 Verificando memoria...";
+    estado.innerHTML = "Verificando memoria...";
 
     barra.style.display = "block";
 
     let porcentaje = 0;
 
     const intervalo = setInterval(() => {
-
         porcentaje++;
-
         progreso.style.width = porcentaje + "%";
 
         if (porcentaje >= 100) {
-
             clearInterval(intervalo);
-
             estado.innerHTML = "✅ Acceso autorizado<br>Bienvenida al universo ❤️";
 
             setTimeout(() => {
                 window.location.href = "universo.html";
             }, 2000);
-
         }
-
     }, 40);
-
 }
